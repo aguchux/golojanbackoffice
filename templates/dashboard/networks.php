f2b855a98e48ffae9a3e720df5111dfae496cb55
uchman005 <obiefunamarcel@gmail.com>
2021-02-04 18:52:49 +0100

    Tried to solve unchecking a product when it's not added in base.js; test to ensure proper function , removed a Template->debug statement I found in ajax.php, setting the grid to col-1 after the 4th level

--- templates/dashboard/networks.php
+++ templates/dashboard/networks.php
@@ -1,7 +1,8 @@
 <div class="section mt-3 text-center">
     <div class="avatar-section">
         <a href="javascript:;" class="btn-link">
-            <img src="<?= $UserInfo->avatar ?>" alt="<?= $UserInfo->fullname ?>" class="imaged w100 rounded"><br /><?= "{$UserInfo->fullname}<br/>({$UserInfo->accid})" ?>
+            <img src="<?= $UserInfo->avatar ?>" alt="<?= $UserInfo->fullname ?>"
+                class="imaged w100 rounded"><br /><?= "{$UserInfo->fullname}<br/>({$UserInfo->accid})" ?>
         </a>
     </div>
 </div>
@@ -14,13 +15,14 @@
 
         <?php foreach ($Level1 as $level1) : $L1U = $Core->UserInfo($level1); ?>
 
-            <div class="col-6">
-                <div class="avatar-section">
-                    <a href="javascript:;" class="btn-link">
-                        <img src="<?= $L1U->avatar ?>" alt="<?= $L1U->fullname ?>" class="imaged w100 rounded"><br /><?= "{$L1U->fullname}<br/>({$L1U->accid})" ?>
-                    </a>
-                </div>
+        <div class="col-6">
+            <div class="avatar-section">
+                <a href="javascript:;" class="btn-link">
+                    <img src="<?= $L1U->avatar ?>" alt="<?= $L1U->fullname ?>"
+                        class="imaged w100 rounded"><br /><?= "{$L1U->fullname}<br/>({$L1U->accid})" ?>
+                </a>
             </div>
+        </div>
 
         <?php endforeach; ?>
 
@@ -31,13 +33,14 @@
 
         <?php foreach ($Level2 as $level2) : $L2U = $Core->UserInfo($level2); ?>
 
-            <div class="col-3">
-                <div class="avatar-section">
-                    <a href="javascript:;" class="btn-link">
-                        <img src="<?= $L2U->avatar ?>" alt="<?= $L2U->fullname ?>" class="imaged w100 rounded"><br /><?= "{$L2U->fullname}<br/>({$L2U->accid})" ?>
-                    </a>
-                </div>
+        <div class="col-3">
+            <div class="avatar-section">
+                <a href="javascript:;" class="btn-link">
+                    <img src="<?= $L2U->avatar ?>" alt="<?= $L2U->fullname ?>"
+                        class="imaged w100 rounded"><br /><?= "{$L2U->fullname}<br/>({$L2U->accid})" ?>
+                </a>
             </div>
+        </div>
 
         <?php endforeach; ?>
 
@@ -48,13 +51,14 @@
 
         <?php foreach ($Level3 as $level3) : $L3U = $Core->UserInfo($level3); ?>
 
-            <div class="col-3">
-                <div class="avatar-section">
-                    <a href="javascript:;" class="btn-link">
-                        <img src="<?= $L3U->avatar ?>" alt="<?= $L3U->fullname ?>" class="imaged w100 rounded"><br /><?= "{$L3U->fullname}<br/>({$L3U->accid})" ?>
-                    </a>
-                </div>
+        <div class="col-3">
+            <div class="avatar-section">
+                <a href="javascript:;" class="btn-link">
+                    <img src="<?= $L3U->avatar ?>" alt="<?= $L3U->fullname ?>"
+                        class="imaged w100 rounded"><br /><?= "{$L3U->fullname}<br/>({$L3U->accid})" ?>
+                </a>
             </div>
+        </div>
 
         <?php endforeach; ?>
 
@@ -65,13 +69,14 @@
 
         <?php foreach ($Level4 as $level4) : $L4U = $Core->UserInfo($level4); ?>
 
-            <div class="col-3">
-                <div class="avatar-section">
-                    <a href="javascript:;" class="btn-link">
-                        <img src="<?= $L4U->avatar ?>" alt="<?= $L4U->fullname ?>" class="imaged w100 rounded"><br /><?= "{$L4U->fullname}<br/>({$L4U->accid})" ?>
-                    </a>
-                </div>
+        <div class="col-2">
+            <div class="avatar-section">
+                <a href="javascript:;" class="btn-link">
+                    <img src="<?= $L4U->avatar ?>" alt="<?= $L4U->fullname ?>"
+                        class="imaged w100 rounded"><br /><?= "{$L4U->fullname}<br/>({$L4U->accid})" ?>
+                </a>
             </div>
+        </div>
 
         <?php endforeach; ?>
 
@@ -80,39 +85,41 @@
 
     <hr>
 
-<div class="row">
+    <div class="row">
 
-    <?php foreach ($Level5 as $level5) : $L5U = $Core->UserInfo($level5); ?>
+        <?php foreach ($Level5 as $level5) : $L5U = $Core->UserInfo($level5); ?>
 
-        <div class="col-3">
+        <div class="col-1">
             <div class="avatar-section">
                 <a href="javascript:;" class="btn-link">
-                    <img src="<?= $L5U->avatar ?>" alt="<?= $L5U->fullname ?>" class="imaged w100 rounded"><br /><?= "{$L5U->fullname}<br/>({$L5U->accid})" ?>
+                    <img src="<?= $L5U->avatar ?>" alt="<?= $L5U->fullname ?>"
+                        class="imaged w100 rounded"><br /><?= "{$L5U->fullname}<br/>({$L5U->accid})" ?>
                 </a>
             </div>
         </div>
 
-    <?php endforeach; ?>
+        <?php endforeach; ?>
 
-</div>
+    </div>
 
 
-<hr>
+    <hr>
 
-<div class="row">
+    <div class="row">
 
-    <?php foreach ($Level6 as $level6) : $L6U = $Core->UserInfo($level6); ?>
+        <?php foreach ($Level6 as $level6) : $L6U = $Core->UserInfo($level6); ?>
 
-        <div class="col-3">
+        <div class="col-1">
             <div class="avatar-section">
                 <a href="javascript:;" class="btn-link">
-                    <img src="<?= $L6U->avatar ?>" alt="<?= $L6U->fullname ?>" class="imaged w100 rounded"><br /><?= "{$L6U->fullname}<br/>({$L6U->accid})" ?>
+                    <img src="<?= $L6U->avatar ?>" alt="<?= $L6U->fullname ?>"
+                        class="imaged w100 rounded"><br /><?= "{$L6U->fullname}<br/>({$L6U->accid})" ?>
                 </a>
             </div>
         </div>
 
-    <?php endforeach; ?>
+        <?php endforeach; ?>
 
-</div>
+    </div>
 
 </div>
