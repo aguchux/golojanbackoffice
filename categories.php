2ba947a3d6719ee5e7cbb8cce3282262bd5d0597
uchman005 <obiefunamarcel@gmail.com>
2021-02-10 20:01:30 +0100

    Finished */?*

--- categories.php
+++ categories.php
@@ -1,408 +1,407 @@
-1 Computers and Accessories, 0
-2 Phones and Tablets, 0
+*1, Computers and Accessories, 0"
+*2, Phones and Tablets, 0"
 
-3 Electronics, 0
-4 Styles and Fashion, 0
-5 Home and Kitchen, 0
-6 Baby, Kids and Toys, 0
-7 Other Categories, 0
+*3, Electronics, 0"
+*4, Styles and Fashion, 0"
+*5, Home and Kitchen, 0"
+*6, Baby, Kids and Toys, 0"
+*7, Other Categories, 0"
 
 
-8 Laptops, 1
+*8, Laptops, 1"
 
-811, Mini Laptops and Netbooks, 8
-812, Notebooks, 8
-813, Ultrabooks, 8
-814, Hybrid PCs, 8
-815, Macbooks, 8
+*811, Mini Laptops and Netbooks, 8"
+*812, Notebooks, 8"
+*813, Ultrabooks, 8"
+*814, Hybrid PCs, 8"
+*815, Macbooks, 8"
 
-9, Desktop and Monitors, 1
+*9, Desktop and Monitors, 1"
 
-911, CPUs, 9
-912, All In Ones, 9
-913, Monitors, 9
-914, UPS, 9
-915, Servers, 9
-916, Desktop Bundles, 9
+*911, CPUs, 9"
+*912, All In Ones, 9"
+*913, Monitors, 9"
+*914, UPS, 9"
+*915, Servers, 9"
+*916, Desktop Bundles, 9"
 
-10, Computing Accessories, 1
+*10, Computing Accessories, 1"
 
-1011, Computer Peripherals, 10
-1012, Bags, Cases, Covers & Sleeves, 10
-1013, Laptop & Desktop Accessories, 10
-1014, Storage Devices, 10
+*1011, Computer Peripherals, 10"
+*1012, Bags, Cases, Covers & Sleeves, 10"
+*1013, Laptop & Desktop Accessories, 10"
+*1014, Storage Devices, 10"
 
-11, Printers, Scanners & Accessories, 1
+*11, Printers, Scanners & Accessories, 1"
 
-1111, Printers, 11
-1112, Scanners, 11
-1113, Inks, Toners & Cartridges, 11
+*1111, Printers, 11"
+*1112, Scanners, 11"
+*1113, Inks, Toners & Cartridges, 11"
 
-12, Wifi & Networking, 1
+*12, Wifi & Networking, 1"
 
-1211, Switches, 12
-1212, Routers, 12
-1213, Modems, 12
-1214, Networking Peripherals, 12
+*1211, Switches, 12"
+*1212, Routers, 12"
+*1213, Modems, 12"
+*1214, Networking Peripherals, 12"
 
-13, PC Gaming, 1
+*13, PC Gaming, 1"
 
-1311, PC Games, 13
-1312, PC Gaming Accessories, 13
+*1311, PC Games, 13"
+*1312, PC Gaming Accessories, 13"
 
+*14, Software, 1"
 
-14, Software, 1
+*1411, Office & Business, 14"
+*1412, Operating Systems, 14"
+*1413, Security & Utilities, 14"
 
-1411, Office & Business, 14
-1412,Operating Systems, 14
-1413,Security & Utilities, 14
+*15, Projectors, 1"
 
-15, Projectors, 1
 
+*16, Mobile Phones, 2"
 
-16, Mobile Phones, 2
+*1621, Smartphones, 16"
+*1622, Feature Phones, 16"
 
-1621, Smartphones, 16
-1622, Feature Phones, 16
+*17, Mobile Phone Accessories, 2"
 
-17, Mobile Phone Accessories, 2
+*1721, Cables, 17"
+*1722, Cases & Covers, 17"
+*1723, Screen Protectors, 17"
+*1724, Chargers & Power Banks, 17"
+*1725, Earphones & Headsets, 17"
+*1726, Smartwatches & Bands, 17"
+*1727, Clips, Holders & Stands, 17"
+*1728, Batteries, 17"
 
-1721, Cables, 17
-1722, Cases & Covers, 17
-1723, Screen Protectors, 17
-1724, Chargers & Power Banks, 17
-1725, Earphones & Headsets, 17
-1726, Smartwatches & Bands, 17
-1727, Clips, Holders & Stands, 17
-1728, Batteries, 17
+*18, Tablets, 2"
 
-18, Tablets, 2
+*1821, Android, 18"
+*1822,iOS, 18"
+*1823,Windows, 18"
+*1824,Other OS', 18"
 
-1821, Android, 18
-1822,iOS, 18
-1823,Windows, 18
-1824,Other OS', 18
+*19, Tablet Accessories, 2"
 
-19, Tablet Accessories, 2
+*1921, Cases & Covers, 19"
+*1922, Holders & Stands, 19"
+*1923, Other Accessories, 19"
 
-1921, Cases & Covers, 19
-1922, Holders & Stands, 19
-1923, Other Accessories, 19
+*20, Televisions, 3"
 
-20, Televisions, 3
+*2031, Smart TVs, 3"
+*2032, LED TVs, 3"
+*2033, Curved TVs, 3"
+*2034, OLED TVs, 3"
+*2035, Plasma TVs, 3"
 
-2031, Smart TVs, 3
-2032, LED TVs, 3
-2033, Curved TVs, 3
-2034, OLED TVs, 3
-2035, Plasma TVs, 3
+*21, DVD Players and Recorders, 3"
 
-21, DVD Players and Recorders, 3
+*2131, DVD Players, 21"
+*2132, DVD Recorders, 21"
+
+*22, Cameras, 3"
+
+*2231, Digital Cameras, 22"
+*2232, Professional & SLR Cameras, 22"
+*2233, Camcorders & Video Cameras, 22"
+*2234, Camera Lenses & Accessories, 22"
+*2235, CCTV Cameras, 22"
+
+23, Accessories, 3"
+
+2331, TV Audio, 23"
+2332, Headphones, 23"
+2333, Television Accessories, 23"
+2334, Other Accessories, 23"
+2335, Gaming Accessories, 23"
+
+*24, Home Theatres & Audio Systems, 3"
+
+*2431, Home Theatre, 24"
+*2432, HiFi Systems, 24"
+*2433, MP3 Players & Speakers, 24"
+
+*25, Games and Consoles, 3"
+
+*2531, PS4, 25"
+*2532,PS3, 25"
+*2533,Xbox One, 25"
+*2534,Xbox 360, 25"
+*2535,Nintendo Wii, 25"
+*2536,Sony PSP, 25"
+*2537,PS Vita, 25"
+*2538,Nintendo 3DS, 25"
 
-2131, DVD Players, 21
-2132, DVD Recorders, 21
-
-22, Cameras, 3
-
-2231, Digital Cameras, 22
-2232, Professional & SLR Cameras, 22
-2233, Camcorders & Video Cameras, 22
-2234, Camera Lenses & Accessories, 22
-2235, CCTV Cameras, 22
-
-23, Accessories, 3
-
-2331, TV Audio, 23
-2332, Headphones, 23
-2333, Television Accessories, 23
-2334, Other Accessories, 23
-2335, Gaming Accessories, 23
-
-24, Home Theatres & Audio Systems, 3
-
-2431, Home Theatre, 24
-2432, HiFi Systems, 24
-2433, MP3 Players & Speakers, 24
-
-25, Games and Consoles, 3
+*26, Women's Wear, 4"
 
-2531, PS4, 25
-2532,PS3, 25
-2533,Xbox One, 25
-2534,Xbox 360, 25
-2535,Nintendo Wii, 25
-2536,Sony PSP, 25
-2537,PS Vita, 25
-2538,Nintendo 3DS, 25
+*2641, Dresses, 26"
+*2642, Tops, 26"
+*2643, Trousers, 26"
+*2644, Jumpsuits & Playsuits, 26"
+*2645, Suits & Blazers, 26"
+*2646, Skirts, 26"
+*2647, Co-ordinates, 26"
+*2648, Lingerie & Sleepwear, 26"
+*2649, Ready to Wear, 26"
 
-26, Women's Wear, 4
+*27, Women's Shoes, 4"
 
-2641, Dresses, 26
-2642, Tops, 26
-2643, Trousers, 26
-2644, Jumpsuits & Playsuits, 26
-2645, Suits & Blazers, 26
-2646, Skirts, 26
-2647, Co-ordinates, 26
-2648, Lingerie & Sleepwear, 26
-2649, Ready to Wear, 26
-
-27, Women's Shoes, 4
-
-2741, Ballerinas & Flats, 27
-2742, Heels, 27
-2743, Sandals & Slippers, 27
-2744, Wedges, 27
-2745, Sport Shoes, 27
-2746, Shoes & Bags, 27
-
-28, Women's Accessories, 4
-
-2841, Bags, 28
-2842, Belts, 28
-2843, Purses & Clutches, 28
-2844, Wallets, 28
-2845, Jewellery, 28
-2846, Hats & Scarves, 28
-
-29, Men's Wear, 4
-
-2941, Shirts, 29
-2942, Polos, 29
-2943, T-Shirts, 29
-2944, Jeans, 29
-2945, Trousers & Shorts, 29
-2946, Suits, Blazers & Jackets, 29
-2947, Pyjamas, 29
-2948, Jerseys, 29
-2949, Traditional Wear, 29
-
-30, Men's Shoes, 4
-
-3041, Casual Shoes, 30
-3042, Formal Shoes, 30
-3043, Slippers & Sandals, 30
-3044, Shoe Care & Accessories, 30
-
-31, Men's Accessories, 4
-
-3141, Belts & Wallets, 31
-3142, Socks & Underwear, 31
-3143, Caps & Hats, 31
-3144, Jewellery, 31
-3145, Bags, 31
-3146, Ties & Cufflinks, 31
-
-32, Watches, 4
-
-3241, Men's Watches, 32
-3242, Women's Watches, 32
-3243, Unisex Watches, 32
-
-33, Fashion Bundles, 4
-
-3341, Women's Fashion Bundles, 33
-3342, Men's Fashion B, 33
-
-34, Style Finder for Women, 4
-
-3441, Monochrome, 34
-3442, Floral, 34
-3443, Bold in Black, 34
-3444, 9 to 5 Chic, 34
-3445, Trending Now, 34
-3446, Red Hot, 34
-
-35, Style Finder for Men, 4
-
-3541, Monochrome, 35
-3542, Prints, 35
-3543, Wardrobe Basics, 35
-3544, Men in Blue, 35
-3545, Business Look, 35
-
-36, Plus Size, 4
-
-37, Under 5K Shop, 4
-
-3741, Men's Fashion Under 5K, 37
-3742, Women's Fashion Under 5K, 37
-
-38, Wedding Shop, 4
-
-3841, Women's Wedding Shop, 38
-3842, Men's Wedding Shop, 38
-
-39, Textiles, 4
-
-40, Large Appliances, 5
-
-4051, Air Conditioners & Coolers, 40
-4052, Fans, 40
-4053, Freezers, 40
-4054, Washers & Dryers, 40
-4055, Refrigerators, 40
-4056, Cookers & Ovens, 40
-4057, Water Dispensers, 40
-4058, Vacuum Cleaners, 40
-
-41, Small Appliances, 5
-
-4151, Blenders, Juicers & Mixers, 41
-4152, Hot Plates & Burners, 41
-4153, Irons & Steamers, 41
-4154, Processors & Mincers, 41
-4155, Toasters & Sandwich Makers, 41
-4156, Deep Fryers & Rice Cookers, 41
-4157, Electric Kettles, 41
-4158, Microwaves, 41
-4159, Yam Pounder, 41
-
-42, Home Furnishings, 5
-
-4251, Bed & Bathroom Furnishings, 42
-4252, Curtains & Blinds, 42
-4253, Decor, 42
-4254, Light Fixtures, 42
-4255, Rugs & Carpets, 42
-4256, Housekeeping & Pet Supplies, 42
-
-43, Top Brands, 5
-
-4351, LG, 43
-4352, Samsung, 43
-4353, Polystar, 43
-4354, Scanfrost, 43
-4355, Hisense, 43
-4356, Saisho, 43
-4357, Haier Thermocool, 43
-4358, Master Chef, 43
-
-44, Kitchen & Dining, 5
-
-4451, Cook and Bakeware, 44
-4452, Dining, 44
-4453, Kitchen Utensils, 44
-4454, Cooker Hoods & Ventilators, 44
-
-45, Furniture, 5
-
-4551, Living Room Furniture, 45
-4552, Bedroom Furniture, 45
-4553, Office Furniture, 45
-4554, Kitchen & Dining Furniture, 45
-
-46, Others, 5
-
-4651, Umbrellas, 46
-4652, Rain Boots & Raincoats, 46
-4653, Gas Cylinder & Accessories, 46
-4654, Towel Racks, 46
-4655, Garment Steamers, 46
-
-47, Accessories for Small & Large Appliances, 5
-
-48, Home & Kitchen Bundles, 5
-
-49, Appliances With Special Offers, 5
-
-50, Fashion for Girls, 6 
-
-5060, Fashion for Girls, 50
-5061, Sets, 50
-5062, Dresses, 50
-5063, Tops, Jackets, & Sweatshirts, 50
-5064, Denim, Trousers & Leggings, 50
-5065, Underwear & Socks, 50
-5066, Watches, 50
-5067, Shoes, 50
-5068, Sleepwear, 50
-5069, Bodysuits & Playsuits, 50
-
-51, Fashion for Boys, 6 
-
-5161, Baby Essentials, 51
-5162, Maternity, 51
-5163, School Store, 51
-5164, Travel & Safety Gear, 51 
-5165, Diapering & Daily Care, 51
-5166, Kids' Beddings & Decor, 51
-
-52, Beauty, Health & Personal Care, 7
-
-5271, Makeup, 52
-5272, Fragrances, 52
-5273, Hair Centre, 52
-5274, Health, 52
-5275, Personal Care, 52
-5276, Skin Care, 52
-5277, Sexual Wellness, 52
-5278, Contraceptives, 52
-5279, Other Contraceptives & Lubricants, 52
-
-53, Sports and Fitness, 7
-
-5371, Fitness, 53
-5372, Outdoor & Indoor Games, 53
-5373, Sportswear, 53
-5374, Football, 53
-5375, Swimming, 53
-5376, Boxing, 53
-5377, Basketball, 53
-
-54, Books & Media Library, 7
-
-5471, Books, 54
-5472, Musical Equipment, 54
-5473, Keyboard, Pianos & Drums, 54
-5474, Stage, Studio & Recording Equipment, 54
-5475, Wind Instruments, 54
-5476, String Instruments, 54
-5477, Audio Books, 54
-5478, African Tales, 54
-
-55, Alcoholic Beverages, 7
-
-5571, Wines, 55
-5572, Spirits, 55
-5573, Liqueurs & Creams, 55
-5574, Whiskey, 55
-5575, Champagne, 55
-
-56, Office & School Supplies, 7
-
-5671, Greeting Cards, 56
-5672, School Supplies, 56
-5673, Office Supplies, 56
-5674, Build Your Office, 56
-
-57, Generators & Power Solutions, 7
-
-5771, Generators & Accessories, 57
-5772, Inverters, 57
-5773, UPS & Surge Protectors, 57
-5774, Solar & Alternative Energy, 57
-
-58, Automotive, 7
-
-5871, Automotive Tools & Accessories, 58
-5872, Safety and Security, 58
-5873, Autocare & Maintenance, 58
-5874, Tyres & Batteries, 58
-5875, Replacement Parts, 58
-5876, Hand & Power Tools, 58
-
-59, Building & Industrial, 7
-
-5971, Sewing Machines & Accessories, 59
-5972, Tools, 59
-5973, Plumbing Materials, 59
-5974, Construction Materials, 59
-5975, Paints, 59
-5976, Electrical Fittings, 59
-
-60, Groceries, 7
-
-6071, Beverages, 60
-6072, Food, 60
-6073, Frozen Food, 60
+*2741, Ballerinas & Flats, 27"
+*2742, Heels, 27"
+*2743, Sandals & Slippers, 27"
+*2744, Wedges, 27"
+*2745, Sport Shoes, 27"
+*2746, Shoes & Bags, 27"
+
+*28, Women's Accessories, 4"
+
+*2841, Bags, 28"
+*2842, Belts, 28"
+*2843, Purses & Clutches, 28"
+*2844, Wallets, 28"
+*2845, Jewellery, 28"
+*2846, Hats & Scarves, 28"
+
+*29, Men's Wear, 4"
+
+*2941, Shirts, 29"
+*2942, Polos, 29"
+*2943, T-Shirts, 29"
+*2944, Jeans, 29"
+*2945, Trousers & Shorts, 29"
+*2946, Suits, Blazers & Jackets, 29"
+*2947, Pyjamas, 29"
+*2948, Jerseys, 29"
+*2949, Traditional Wear, 29"
+
+*30, Men's Shoes, 4"
+
+*3041, Casual Shoes, 30"
+*3042, Formal Shoes, 30"
+*3043, Slippers & Sandals, 30"
+*3044, Shoe Care & Accessories, 30"
+
+*31, Men's Accessories, 4"
+
+*3141, Belts & Wallets, 31"
+*3142, Socks & Underwear, 31"
+*3143, Caps & Hats, 31"
+*3144, Jewellery, 31"
+*3145, Bags, 31"
+*3146, Ties & Cufflinks, 31"
+
+*32, Watches, 4"
+
+*3241, Men's Watches, 32"
+*3242, Women's Watches, 32"
+*3243, Unisex Watches, 32"
+
+*33, Fashion Bundles, 4"
+
+*3341, Women's Fashion Bundles, 33"
+*3342, Men's Fashion B, 33"
+
+*34, Style Finder for Women, 4"
+
+*3441, Monochrome, 34"
+*3442, Floral, 34"
+*3443, Bold in Black, 34"
+*3444, 9 to 5 Chic, 34"
+*3445, Trending Now, 34"
+*3446, Red Hot, 34"
+
+*35, Style Finder for Men, 4"
+
+*3541, Monochrome, 35"
+*3542, Prints, 35"
+*3543, Wardrobe Basics, 35"
+*3544, Men in Blue, 35"
+*3545, Business Look, 35"
+
+*36, Plus Size, 4"
+
+*37, Under 5K Shop, 4"
+
+*3741, Men's Fashion Under 5K, 37"
+*3742, Women's Fashion Under 5K, 37"
+
+*38, Wedding Shop, 4"
+
+*3841, Women's Wedding Shop, 38"
+*3842, Men's Wedding Shop, 38"
+
+*39, Textiles, 4"
+
+*40, Large Appliances, 5"
+
+*4051, Air Conditioners & Coolers, 40"
+*4052, Fans, 40"
+*4053, Freezers, 40"
+*4054, Washers & Dryers, 40"
+*4055, Refrigerators, 40"
+*4056, Cookers & Ovens, 40"
+*4057, Water Dispensers, 40"
+*4058, Vacuum Cleaners, 40"
+
+*41, Small Appliances, 5"
+
+*4151, Blenders, Juicers & Mixers, 41"
+*4152, Hot Plates & Burners, 41"
+*4153, Irons & Steamers, 41"
+*4154, Processors & Mincers, 41"
+*4155, Toasters & Sandwich Makers, 41"
+*4156, Deep Fryers & Rice Cookers, 41"
+*4157, Electric Kettles, 41"
+*4158, Microwaves, 41"
+*4159, Yam Pounder, 41"
+
+*42, Home Furnishings, 5"
+
+*4251, Bed & Bathroom Furnishings, 42"
+*4252, Curtains & Blinds, 42"
+*4253, Decor, 42"
+*4254, Light Fixtures, 42"
+*4255, Rugs & Carpets, 42"
+*4256, Housekeeping & Pet Supplies, 42"
+
+*43, Top Brands, 5"
+
+*4351, LG, 43"
+*4352, Samsung, 43"
+*4353, Polystar, 43"
+*4354, Scanfrost, 43"
+*4355, Hisense, 43"
+*4356, Saisho, 43"
+*4357, Haier Thermocool, 43"
+*4358, Master Chef, 43"
+
+*44, Kitchen & Dining, 5"
+
+*4451, Cook and Bakeware, 44"
+*4452, Dining, 44"
+*4453, Kitchen Utensils, 44"
+*4454, Cooker Hoods & Ventilators, 44"
+
+*45, Furniture, 5"
+
+*4551, Living Room Furniture, 45"
+*4552, Bedroom Furniture, 45"
+*4553, Office Furniture, 45"
+*4554, Kitchen & Dining Furniture, 45"
+
+*46, Others, 5"
+
+*4651, Umbrellas, 46"
+*4652, Rain Boots & Raincoats, 46"
+*4653, Gas Cylinder & Accessories, 46"
+*4654, Towel Racks, 46"
+*4655, Garment Steamers, 46"
+
+*47, Accessories for Small & Large Appliances, 5"
+
+*48, Home & Kitchen Bundles, 5"
+
+*49, Appliances With Special Offers, 5"
+
+*50, Fashion for Girls, 6"
+
+*5060, Fashion for Girls, 50"
+*5061, Sets, 50"
+*5062, Dresses, 50"
+*5063, Tops, Jackets, & Sweatshirts, 50"
+*5064, Denim, Trousers & Leggings, 50"
+*5065, Underwear & Socks, 50"
+*5066, Watches, 50"
+*5067, Shoes, 50"
+*5068, Sleepwear, 50"
+*5069, Bodysuits & Playsuits, 50"
+
+*51, Fashion for Boys, 6"
+
+*5161, Baby Essentials, 51"
+*5162, Maternity, 51"
+*5163, School Store, 51"
+*5164, Travel & Safety Gear, 51" 
+*5165, Diapering & Daily Care, 51"
+*5166, Kids' Beddings & Decor, 51"
+
+*52, Beauty, Health & Personal Care, 7"
+
+*5271, Makeup, 52"
+*5272, Fragrances, 52"
+*5273, Hair Centre, 52"
+*5274, Health, 52"
+*5275, Personal Care, 52"
+*5276, Skin Care, 52"
+*5277, Sexual Wellness, 52"
+*5278, Contraceptives, 52"
+*5279, Other Contraceptives & Lubricants, 52"
+
+*53, Sports and Fitness, 7"
+
+*5371, Fitness, 53"
+*5372, Outdoor & Indoor Games, 53"
+*5373, Sportswear, 53"
+*5374, Football, 53"
+*5375, Swimming, 53"
+*5376, Boxing, 53"
+*5377, Basketball, 53"
+
+*54, Books & Media Library, 7"
+
+*5471, Books, 54"
+*5472, Musical Equipment, 54"
+*5473, Keyboard, Pianos & Drums, 54"
+*5474, Stage, Studio & Recording Equipment, 54"
+*5475, Wind Instruments, 54"
+*5476, String Instruments, 54"
+*5477, Audio Books, 54"
+*5478, African Tales, 54"
+
+*55, Alcoholic Beverages, 7"
+
+*5571, Wines, 55"
+*5572, Spirits, 55"
+*5573, Liqueurs & Creams, 55"
+*5574, Whiskey, 55"
+*5575, Champagne, 55"
+
+*56, Office & School Supplies, 7"
+
+*5671, Greeting Cards, 56"
+*5672, School Supplies, 56"
+*5673, Office Supplies, 56"
+*5674, Build Your Office, 56"
+
+*57, Generators & Power Solutions, 7"
+
+*5771, Generators & Accessories, 57"
+*5772, Inverters, 57"
+*5773, UPS & Surge Protectors, 57"
+*5774, Solar & Alternative Energy, 57"
+
+*58, Automotive, 7"
+
+*5871, Automotive Tools & Accessories, 58"
+*5872, Safety and Security, 58"
+*5873, Autocare & Maintenance, 58"
+*5874, Tyres & Batteries, 58"
+*5875, Replacement Parts, 58"
+*5876, Hand & Power Tools, 58"
+
+*59, Building & Industrial, 7"
+
+*5971, Sewing Machines & Accessories, 59"
+*5972, Tools, 59"
+*5973, Plumbing Materials, 59"
+*5974, Construction Materials, 59"
+*5975, Paints, 59"
+*5976, Electrical Fittings, 59"
+
+*60, Groceries, 7"
+
+*6071, Beverages, 60"
+*6072, Food, 60"
+*6073, Frozen Food, 60"
